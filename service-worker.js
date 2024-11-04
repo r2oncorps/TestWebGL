17fd34f516616bf0dc1d98d022e78d1250dd84ff
jjangsupil <jjangsupil@monoverse.io>
2024-11-04 17:46:29 +0900

    버전 테스트

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+
+const CACHE_NAME = 'cache-v1.0.0';
+const urlsToCache = [
+    './',
+    './index.html',
+    './TemplateData/style.css',
+    './Build/Build.data',
+    './Build/Build.framework.js',
+    './Build/Build.loader.js',
+    './Build/Build.wasm'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
